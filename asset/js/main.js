195a48e85f50c1875c0b448fbc4d1944a6537364
thomas.dns <dasneves.thomas.sio@gmail.com>
2023-10-20 12:05:00 +0200

    Completed task

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,51 +1,78 @@
 function addTodo() {
-    const todoInput = document.getElementById("todoInput");
-    const todoList = document.getElementById("todoList");
-    if (todoInput.value.trim() === "") {
-      alert("Veuillez entrer une tâche!");
-      return;
-    }
-    const todoText = todoInput.value;
-    if (todoText) {
-      const todoEl = document.createElement("li");
-      todoEl.innerText = todoText;
-      // Ajouter le bouton de modification
-      const editBtn = document.createElement("button");
-      editBtn.classList = 'edit';
-      editBtn.innerText = "Modifier";
-      editBtn.onclick = function () {
-        editTodo(todoEl);
-      };
-      todoEl.appendChild(editBtn);
-      todoList.appendChild(todoEl);
-      todoInput.value = "";
-    }
+  const todoInput = document.getElementById("todoInput");
+  const todoList = document.getElementById("todoList");
+  if (todoInput.value.trim() === "") {
+    alert("Veuillez entrer une tâche!");
+    return;
   }
-  function editTodo(todoEl) {
-    const input = document.createElement("input");
-    input.type = "text";
-    input.value = todoEl.innerText;
-    // Remplace le texte de la tâche par l'input
-    todoEl.innerHTML = "";
-    todoEl.appendChild(input);
-    input.focus();
-    // Une fois la tâche modifiée (après appui sur "Entrée" ou clic en dehors)
-    input.onblur = finishEditing;
-    input.onkeydown = function(e) {
-        if (e.key === "Enter") {
-            finishEditing.call(input); // Utilisez "call" pour définir "this" à l'intérieur de finishEditing
-        }
+
+  const todoText = todoInput.value;
+  if (todoText) {
+    const todoEl = document.createElement("li");
+
+    const todoTextEl = document.createElement("span");
+    todoTextEl.innerText = todoText;
+    todoTextEl.onclick = function () {
+      toggleCompleted(todoTextEl);
     };
-    function finishEditing() {
-        const newText = this.value;
-        todoEl.innerHTML = newText;
-        // Ajouter à nouveau le bouton de modification
-        const editBtn = document.createElement("button");
-        editBtn.innerText = "Modifier";
-        editBtn.classList = 'edit';
-        editBtn.onclick = function() {
-            editTodo(todoEl);
-        };
-        todoEl.appendChild(editBtn);
-    }
+    todoEl.appendChild(todoTextEl);
+
+    const editBtn = document.createElement("button");
+    editBtn.classList = "edit";
+    editBtn.innerText = "Modifier";
+    editBtn.onclick = function () {
+      editTodo(todoEl);
+    };
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Supprimer";
+    deleteBtn.onclick = function () {
+      deleteTodo(todoEl);
+    };
+    todoEl.appendChild(deleteBtn);
+    todoEl.appendChild(editBtn);
+    todoList.appendChild(todoEl);
+    todoInput.value = "";
   }
+}
+
+function editTodo(todoEl) {
+  const input = document.createElement("input");
+  input.type = "text";
+  input.value = todoEl.innerText;
+
+  todoEl.innerHTML = "";
+  todoEl.appendChild(input);
+  input.focus();
+
+  input.onblur = finishEditing;
+  input.onkeydown = function (e) {
+    if (e.key === "Enter") {
+      finishEditing.call(input);
+    }
+  };
+  function finishEditing() {
+    const newText = this.value;
+    todoEl.innerHTML = newText;
+    // Ajouter à nouveau le bouton de modification
+    const editBtn = document.createElement("button");
+    editBtn.innerText = "Modifier";
+    editBtn.classList = "edit";
+    editBtn.onclick = function () {
+      editTodo(todoEl);
+    };
+    todoEl.appendChild(editBtn);
+  }
+}
+
+function deleteTodo(todoEl) {
+  todosList.removeChild(todoEl);
+}
+
+function toggleCompleted(todoTextEl) {
+  if (todoTextEl.style.textDecoration === "line-through") {
+    todoTextEl.style.textDecoration = "none";
+  } else {
+    todoTextEl.style.textDecoration = "line-through";
+  }
+}
